0bd5a55c5063a0619cd4f5e45013aa7ac9f5208c
qwepoiasdlkj1229 <liculdakutimoy@gmail.com>
2022-07-31 18:44:25 +0800

    Practice strings

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
-#
+#include <cmath>
 
 using namespace std;
 
+void file1(){
+
+    string file2;
+
+    cout << "Are you kidding me? " ;
+    cin >> file2;
+    cout << " how dare you!";
+
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "luis" ;
+    string hello;
+    string luis;
+    string name;
+    string age;
+
+
+    luis = " im Luis by the way";
+    hello = " Hello how are you? ";
+
+    cout << hello << " " << luis << endl;
+    cout << " what is your name? " ;
+    cin >> name;
+    cout << " hello " << name << " how old are you? ";
+    cin >> age;
+    cout << " so you're " << age << " year(s) old" << endl;
+
+    file1();
+
+
+
+
     return 0;
 }
