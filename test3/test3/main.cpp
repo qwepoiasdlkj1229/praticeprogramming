78adbecf47085b79d83ea2ce9441fbde14879196
qwepoiasdlkj1229 <liculdakutimoy@gmail.com>
2022-08-05 21:21:40 +0800

    Guess Games with for loop

--- test3/test3/main.cpp
+++ test3/test3/main.cpp
@@ -5,25 +5,27 @@ using namespace std;
 int main()
 {
     cout << " you have 3 chances" << endl;
-    int secretNum = 4;
     int guess;
     int guessCount = 0;
     int guessLimit = 3;
     bool outOfGuesses = false;
 
-    while(secretNum != guess && !outOfGuesses){
-        if(guessCount < guessLimit){
-        cout <<" Guess the number 1-10: ";
+
+
+
+    for(int i = 4; i != guess && !outOfGuesses;){
+        if (guessCount < guessLimit){
+        cout <<" Guess the number 1 - 10: ";
         cin >> guess;
         guessCount++;
-
-        } else {
-            outOfGuesses = true;
-        }
+     } else {
+        outOfGuesses = true;
+     }
     }
 
-    if(outOfGuesses){
 
+
+    if(outOfGuesses){
         cout << "You lose!";
     } else{
     cout << "you win!";
